59af6f3ef9dbf46c3d0c36bfcc77eb03454f7f35
queen-x-vee <valentinabuoro4@gmail.com>
2022-11-19 08:28:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './index.css';
+import NavBar from './components/Navbar/NavBar';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+     <NavBar/>
+     <Main/>
+     <Footer/>
+    </>
+   
+  
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ padding: 1rem;
+ position: fixed;
+ width:100%;
+ height: 90px;
+ z-index:10;
+
+`
